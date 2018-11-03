714e00405135e769d9cbfe52202e5cf7e696ad0a
James Cairns <J.A.Cairns@2016.hull.ac.uk>
2018-11-03 14:28:34 +0000

    Making of the player controller

---
+++ underbaked/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 3.5f;
+    public float rotSpeed = 5f;
+
+    private Vector3 dir = Vector3.zero; //Direction of which we are travelling
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //var x = Input.GetAxisRaw("Horizontal") * Time.deltaTime * speed;
+        //var z = Input.GetAxisRaw("Vertical") * Time.deltaTime * speed;
+
+        //transform.Translate(x, 0, z);
+
+        dir.x = Input.GetAxis("Horizontal");
+        dir.z = Input.GetAxis("Vertical");
+
+        //if (dir != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
+        //}
+
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
+        {
+            //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
+            //transform.rotation = Quaternion.LookRotation(dir);
+            if (dir != Vector3.zero)
+                transform.rotation = Quaternion.LookRotation(dir);
+            transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        }
+    }
+}
