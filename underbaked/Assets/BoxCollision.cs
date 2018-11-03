55e30e74bc783979d57a58f1bae9beb27d04313a
Lawrence <lawrenceperrin@gmail.com>
2018-11-03 17:00:41 +0000

    slight changes

--- underbaked/Assets/BoxCollision.cs
+++ underbaked/Assets/BoxCollision.cs
@@ -2,12 +2,7 @@
 
 public class BoxCollision : MonoBehaviour {
 
-    public GameObject splat;
-    private void OnCollisionEnter(Collision collision)
-    {
-        Instantiate(splat, gameObject.transform.position, Quaternion.identity);
-        Destroy(this.gameObject);
-    }
+
 
 
 }
