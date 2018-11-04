5dd8023f78da8665d6d0db24ce6de4623ec37f19
Sam <samsmith@leisure-electronics.co.uk>
2018-11-04 10:39:48 +0000

    Added models + implemented them

--- underbaked/Assets/Scripts/PlayerController.cs
+++ underbaked/Assets/Scripts/PlayerController.cs
@@ -28,8 +28,8 @@ public class PlayerController : MonoBehaviour {
         //{
         //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
         //}
-
-        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
+        if(Input.GetAxisRaw("Horizontal")!=0||Input.GetAxisRaw("Vertical")!=0)
+        //if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.DownArrow))
         {
             //transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(dir), Time.deltaTime * rotSpeed);
             //transform.rotation = Quaternion.LookRotation(dir);
